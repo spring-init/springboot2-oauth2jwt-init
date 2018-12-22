5999229410a80ae0f682015ac0c4dd0ff9350ee7
Gabriel Paz <jgabrielpazs@gmail.com>
2018-12-22 00:18:06 -0600

    JWT TOken Enhanced

--- src/main/java/borrameref/demo/config/security/AuthorizationServerConfig.java
+++ src/main/java/borrameref/demo/config/security/AuthorizationServerConfig.java
@@ -2,11 +2,18 @@ package borrameref.demo.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Configuration
 @EnableAuthorizationServer
@@ -35,7 +42,21 @@ public class AuthorizationServerConfig {
 
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
-        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter(){
+            @Override
+            public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+                User user = (User) authentication.getPrincipal();
+
+                Map<String, Object> info = new LinkedHashMap<String, Object>(accessToken.getAdditionalInformation());
+
+                info.put("role",user.getAuthorities().iterator().next());
+                info.put("expired",user.isAccountNonExpired());
+
+                DefaultOAuth2AccessToken customAccessToken = new DefaultOAuth2AccessToken(accessToken);
+                customAccessToken.setAdditionalInformation(info);
+                return super.enhance(customAccessToken, authentication);
+            }
+        };
         converter.setSigningKey("123");
         return converter;
     }
