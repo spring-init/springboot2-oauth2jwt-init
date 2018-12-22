f34c2abafa550855c0a0e067530356b4d1de1a20
Gabriel Paz <jgabrielpazs@gmail.com>
2018-12-22 00:04:02 -0600

    Oauth2 AuthSErver + JWT Token Symetric

--- src/main/java/borrameref/demo/config/security/AuthorizationServerConfig.java
+++ src/main/java/borrameref/demo/config/security/AuthorizationServerConfig.java
@@ -1,8 +1,12 @@
 package borrameref.demo.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -23,4 +27,16 @@ public class AuthorizationServerConfig {
 //                                  endpoints) throws Exception {
 //        endpoints.authenticationManager(authenticationManagerBean);
 //    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("123");
+        return converter;
+    }
 }
